eeb528e004113543280df6601f2958a6a9623344
stino-x <107426247+stino-x@users.noreply.github.com>
2024-04-05 21:26:16 +0200

    "Refactor checkbox module: remove unused imports, update variable names, and simplify local storage access"

--- src/Modules/checkbox.js
+++ src/Modules/checkbox.js
@@ -1,33 +1,31 @@
-import {
-  ActivityArray, AddToScreen, saveTaskstoLocalStorage, setActivityArray,
-} from './AddtoLocalStorage.js';
+import { saveTaskstoLocalStorage } from './AddtoLocalStorage.js';
+import AddToScreen from '../index.js';
 
+let activityArray = [];
 export const handleCheckboxchange = (event) => {
   const checkbox = event.target;
-  if (checkbox.type === 'checkbox') {
-    const listItem = checkbox.closest('li');
-    const taskId = listItem.getAttribute('id');
-    ActivityArray()[taskId].completed = checkbox.checked;
-    saveTaskstoLocalStorage();
-    if (checkbox.checked) {
-      listItem.querySelector('.text').classList.add('completed');
-    } else {
-      listItem.querySelector('.text').classList.remove('completed');
-    }
-    saveTaskstoLocalStorage();
+  const listItem = checkbox.parentNode.parentNode;
+  const taskId = listItem.getAttribute('id');
+  activityArray[taskId].completed = checkbox.checked;
+  saveTaskstoLocalStorage();
+  if (checkbox.checked) {
+    listItem.querySelector('.text').classList.add('completed');
+  } else {
+    listItem.querySelector('.text').classList.remove('completed');
   }
+  saveTaskstoLocalStorage();
 };
 
 //   activitleCheckboxchange);
 //   activiteTask);
 // deleting all checked tasks
 export const deleteCheckedTasks = () => {
-  setActivityArray(ActivityArray().filter((MyConstructor) => !MyConstructor.completed));
+  activityArray = activityArray.filter((MyConstructor) => !MyConstructor.completed);
   saveTaskstoLocalStorage();
   AddToScreen();
 };
 export const reIndex = () => {
-  ActivityArray().forEach((task, index) => {
+  activityArray.forEach((task, index) => {
     task.index = index + 1;
   });
 };
@@ -38,21 +36,4 @@ export const cLearALL = () => {
   saveTaskstoLocalStorage();
   AddToScreen();
 };
-
-const loadActivityArrayFromLocalStorage = () => {
-  const activityArrayString = localStorage.getItem('activities');
-  if (activityArrayString) {
-    return JSON.parse(activityArrayString);
-  }
-  return [];
-};
-
-const activityArray1 = loadActivityArrayFromLocalStorage();
-
-activityArray1.forEach((task, index) => {
-  const listItem = document.getElementById(index);
-  if (task.completed) {
-    listItem.querySelector('.text').classList.add('completed');
-  }
-});
 //   ALL);
